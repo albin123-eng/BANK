58997052d761d798201fe54ba5d55ae3a713a064
AlbinSoni <albinantonysony81@gmail.com>
2026-05-25 13:46:30 +0100

    Update Database.py

--- Main_Bank/Database.py
+++ Main_Bank/Database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://bank_user:MySimplePass123@localhost:5432/bank_db"
+SQLALCHEMY_DATABASE_URL =""
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
